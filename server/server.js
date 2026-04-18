057a392430e1e5a7877cf32acb0d8bbfbec41100
Nithin0000007 <127509559+Nithin0000007@users.noreply.github.com>
2026-04-19 01:28:44 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -17,9 +17,11 @@ app.get('/', (req, res) => {
   res.send('Backend is on Fireee');
 });
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => console.log("Server running on port 5000"));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.log(err));
